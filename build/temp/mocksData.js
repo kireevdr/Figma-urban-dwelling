f53a8c6e3b80db54c4baaaaa682f9bcc9e6ac6b1
kireevdr <kireevdr@bk.ru>
2024-08-07 01:20:09 +0300

    add build

---
+++ build/temp/mocksData.js
@@ -0,0 +1,71 @@
+'head': {
+    defaults: {
+
+    }
+}
+
+
+
+
+,
+
+__iconsData: {
+    
+        'cart': {
+            width: '21px',
+            height: '27px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'loop': {
+            width: '20px',
+            height: '21px'
+        },
+    
+        'search-normal': {
+            width: '22px',
+            height: '23px'
+        },
+    
+        'search': {
+            width: '20px',
+            height: '21px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'Vector': {
+            width: '21px',
+            height: '27px'
+        },
+    
+},
+
+__pages: [{
+                name: '!main',
+                href: '!main.html',
+                desc: 'Главная',
+             },{
+                name: '404',
+                href: '404.html',
+                desc: '404',
+             },{
+                name: 'index',
+                href: 'index.html',
+                desc: '//',
+             },{
+                name: 'modal',
+                href: 'modal.html',
+                desc: 'Modal',
+             },{
+                name: 'page',
+                href: 'page.html',
+                desc: 'Текстовая страница',
+             }]
---
+++ build/temp/symbols-data-template.js
@@ -0,0 +1,38 @@
+__iconsData: {
+    
+        'cart': {
+            width: '21px',
+            height: '27px'
+        },
+    
+        'close': {
+            width: '12px',
+            height: '12px'
+        },
+    
+        'loop': {
+            width: '20px',
+            height: '21px'
+        },
+    
+        'search-normal': {
+            width: '22px',
+            height: '23px'
+        },
+    
+        'search': {
+            width: '20px',
+            height: '21px'
+        },
+    
+        'up': {
+            width: '13px',
+            height: '15px'
+        },
+    
+        'Vector': {
+            width: '21px',
+            height: '27px'
+        },
+    
+}
